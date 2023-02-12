46f7bc4ef7cc0df8a68058dc409875aa70f8a9fe
Alex Rousseau <alexrousseau@pop-os.localdomain>
2023-02-12 16:43:50 -0500

    Classe Menu

---
+++ menu.cpp
@@ -0,0 +1,145 @@
+#include "menu.h"
+
+int main()
+{
+    Menu menu;
+
+    //menu.loadConfig();
+
+    //cout << menu.get_modeAccelerometer() << endl;
+    //cout << menu.get_screenHeight() << endl;
+    //cout << menu.get_screenWidth() << endl;
+
+    //menu.set_modeAccelerometer();
+    //menu.set_screenHeight(900);
+    //menu.set_screenWidth(1600);
+
+    //menu.saveConfig("config/new config.config");
+
+    return 0;
+}
+
+Menu::Menu()
+{
+    parameters = new Config[NUMBER_OF_PARAMETERS];
+
+    parameters[0].name = "SCREEN_WIDTH";
+    parameters[1].name = "SCREEN_HEIGHT";
+    parameters[2].name = "MODE_ACCELEROMETER";
+    parameters[3].name = "MODE_JOYSTICK";
+}
+
+void Menu::update_data()
+{
+    screenWidth = parameters[0].value;
+    screenHeight = parameters[1].value;
+    modeAccelerometer = parameters[2].value;
+    modeJoystick = parameters[3].value;
+}
+
+void Menu::update_config()
+{
+    parameters[0].value = screenWidth;
+    parameters[1].value = screenHeight;
+    parameters[2].value = modeAccelerometer;
+}
+
+Menu::~Menu() { delete[] parameters; }
+
+bool Menu::loadConfig()
+{
+   std::ifstream file(CONFIG_PATH);
+
+    if (!file.is_open())
+    {
+        std::cout << "Ouverture du fichier config echoue depuis: " << CONFIG_PATH << std::endl;
+        return false;
+    }
+
+    std::string parameter;
+    int value;
+
+    while (!file.eof())
+    {
+        file >> parameter;
+        file >> value;
+
+        for (int i = 0; i < NUMBER_OF_PARAMETERS; i++)
+        {
+            if (parameter == parameters[i].name)
+                parameters[i].value = value;
+        }
+    }
+
+    update_data();
+
+    file.close();
+
+    return true;
+}		
+
+
+bool Menu::saveConfig(std::string location)
+{
+    update_config();
+    
+    std::ofstream file(location);
+
+    if (!file.is_open())
+    {
+        std::cout << "Creation du fichier config echoue a l'adresse: " << location << std::endl;
+        return false;
+    }
+
+    for (int i = 0; i < NUMBER_OF_PARAMETERS; i++)
+    {
+        file << parameters[i].name << " " << parameters[i].value << std::endl;
+    }
+
+    file.close();
+
+    return true;
+}
+
+void Menu::set_screenWidth(int value)
+{
+    screenWidth = value;
+}
+
+
+void Menu::set_screenHeight(int value)
+{
+    screenHeight = value;
+}
+
+void Menu::set_modeAccelerometer()
+{
+    modeAccelerometer = true;
+    modeJoystick = false;
+}
+
+void Menu::set_modeJoystick()
+{
+    modeJoystick = true;
+    modeAccelerometer = false;
+}
+
+int Menu::get_screenWidth()
+{
+    return screenWidth;
+}
+
+int Menu::get_screenHeight()
+{
+    return screenHeight;
+}
+
+bool Menu::get_modeAccelerometer()
+{
+    return modeAccelerometer;
+}
+
+bool Menu::get_modeJoystick()
+{
+    return modeJoystick;
+}
---
+++ menu.h
@@ -0,0 +1,61 @@
+/* La classe Menu permet de gérer les différents paramètres liés à la manette et au jeu.
+
+Elles permet de :
+	-Activer certains composants de la manette ou non (acceleromètre, etc)
+	-Changer certains paramètres du jeu
+	-Loader et sauvegarder une config
+*/
+
+///		Libraries
+#include <iostream>
+#include <fstream>
+///
+
+#ifndef MENU_H
+#define MENU_H
+#define CONFIG_PATH "./config/game.config"		//Location du fichier config
+#define NUMBER_OF_PARAMETERS 4
+
+struct Config
+{
+	std::string name;
+	short value;
+};
+
+class Menu
+{
+private:
+	Config* parameters;
+
+	// Devra contenir les différents paramètres
+	bool modeAccelerometer;
+	bool modeJoystick;
+	int screenWidth;
+	int screenHeight;
+
+public:
+	Menu();
+	~Menu();
+
+	void update_data();
+	void update_config();
+
+
+	bool loadConfig();
+	bool saveConfig(std::string location);
+
+
+	void set_screenWidth(int value);
+	void set_screenHeight(int value);
+	void set_modeAccelerometer();
+	void set_modeJoystick();
+
+
+	int get_screenWidth();
+	int get_screenHeight();
+	bool get_modeAccelerometer();
+	bool get_modeJoystick();
+};
+
+
+#endif
