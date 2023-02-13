02519b32c8315422d015c4ba53e165b4dd6b1c24
olivierstamand <stao0901@usherbrooke.ca>
2023-02-13 11:11:54 -0500

    Updated and moved to my branch

---
+++ Structs.h
@@ -0,0 +1,17 @@
+//
+// Created by stao0901 on 2023-02-13.
+//
+
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+struct Position {
+    int x;
+    int y;
+};
+struct Velocity {
+    int x;
+    int y;
+};
+
+#endif  STRUCTS_H
