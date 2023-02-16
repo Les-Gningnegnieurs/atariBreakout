639ff46a0a9e65e5e5250f9bd537da0258ff0c83
SimonLalan7 <70168528+SimonLalan7@users.noreply.github.com>
2023-02-16 12:40:00 -0500

    ajustement

--- inputs/Controller.h
+++ inputs/Controller.h
@@ -1,13 +1,14 @@
 #ifndef CONTROLLER_H__
 #define CONTROLLER_H__
 
-#include "components/components.h"
+#include "../components/components.h"
 #include <iostream>
-#include <nlohmann/json.hpp>
+#include "../lib/nlohmann/json.hpp"
 using json = nlohmann::json;
 
+
 class Controller{
-private:
+protected:
     Joystick _joystick;
     Bargraph _bargraph;
     LedRGB _leds[2];
@@ -18,6 +19,8 @@ public:
     virtual void receiveInputs();
     virtual void sendOutputs();
 
+    Controller();
+    ~Controller();
     Joystick getJoystick();
     void setBargraph(int id, bool val);
     void setLED(int id, int R, int G, int B);
