2a4e5c0740bfe39b00f8bbde5b19009f377c8220
arous86 <97749798+arous86@users.noreply.github.com>
2023-03-03 18:43:25 -0500

    Update Plateforme.h

--- Plateforme.h
+++ Plateforme.h
@@ -1,10 +1,10 @@
+#ifndef PLATEFORME_H
+#define PLATEFORME_H
+
 #include "Structs.h"
 #include "Balle.h"
 #include <iostream>
 #include <sstream>
-#ifndef PLATEFORME_H
-#define PLATEFORME_H
-
 
 class Plateforme {
 private:
@@ -14,8 +14,6 @@ private:
     int columns;
     int sizeX;
     int sizeY;
-    int tiltAngle;
-    int maxSpeed;
 public:
     Plateforme();
     Plateforme(LevelInfos I);
