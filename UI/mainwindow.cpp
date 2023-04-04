b8941eb93402b8745842057fc9f155b56a4638c9
SimonLalan7 <70168528+SimonLalan7@users.noreply.github.com>
2023-04-04 14:49:59 -0400

    Update mainwindow.cpp

--- UI/mainwindow.cpp
+++ UI/mainwindow.cpp
@@ -1,35 +1,69 @@
 #include <QApplication>
+#include <QMainWindow>
 #include <QPushButton>
 #include <QMessageBox>
 #include <QString>
+#include <QRect>
+#include <QStyle>
 
 
 class CustomBtn : public QPushButton {
 public:
 	CustomBtn(std::string name = "", std::string message = "");
 	void clicked();
+	QMessageBox* getMB();
 private:
 	std::string _name;
 	std::string _msg;
-	QMessageBox _mb;
+	QMessageBox* _mb;
 
 };
+
+class MainWindow : public QMainWindow {
+public:
+	MainWindow(CustomBtn* btn);
+public slots:
+	void handleMbClosed();
+private:
+	CustomBtn* _btn;
+};
+
+MainWindow::MainWindow(CustomBtn* btn) {
+	this->resize(600, 600);
+	_btn = btn;
+	_btn->setParent(this);
+	_btn->setGeometry(15, 15, 150, 50);
+	QMessageBox* mb = _btn->getMB();
+	connect(mb, &QMessageBox::accepted, this, &MainWindow::handleMbClosed);
+	_btn->show();
+}
+
+void MainWindow::handleMbClosed() {
+	setStyleSheet("QMainWindow {background-color: 'green'}");
+	show();
+}
 
 CustomBtn::CustomBtn(std::string name, std::string message) : QPushButton(QString::fromStdString(name)) {
 	_name = name;
 	_msg = message;
+	_mb = new QMessageBox();
 	connect(this, &QPushButton::released, this, &CustomBtn::clicked);
 }
 
 void CustomBtn::clicked() {
-	_mb.setText(QString::fromStdString(_msg));
-	_mb.show();
+	_mb->setText(QString::fromStdString(_msg));
+	_mb->show();
+}
+
+QMessageBox* CustomBtn::getMB() {
+	return _mb;
 }
 
 int main(int argc, char **argv)
 {
  QApplication app (argc, argv);
- CustomBtn button("ouvrir message box", "Ceci est une message box");
- button.show();
+ CustomBtn* button = new CustomBtn("ouvrir message box", "Ceci est une message box");
+ MainWindow m(button);
+ m.show();
  return app.exec();
 }
